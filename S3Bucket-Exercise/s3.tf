df26652b6978a07270b407a842e55cf666abcbcf
CarlosDRomero <cdavid.romeror@gmail.com>
2025-08-28 15:43:37 -0500

    Ejercicio de S3 bucket con id aleatoria

---
+++ S3Bucket-Exercise/s3.tf
@@ -0,0 +1,29 @@
+terraform {
+    required_version = "1.13.0"
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+      }
+      random = {
+        source = "hashicorp/random"
+        version = "~> 3.7"
+      }
+    }
+}
+provider "aws" {
+  region = "us-east-2"
+}
+resource "random_id" "bucket_id" {
+    byte_length = 8
+}
+resource "aws_s3_bucket" "bucket" {
+    bucket = "bucket-${random_id.bucket_id.hex}"
+    tags = {
+      Name="TestBucket"
+    }
+}
+
+output "bucket_name" {
+    value = aws_s3_bucket.bucket.bucket
+}
