1e347b8a285f1f3eb57d9d2bb0cec2be859b5dd2
CarlosDRomero <cdavid.romeror@gmail.com>
2025-09-09 09:21:51 -0500

    Agregando politica de IAM

--- DataSources-Exercises/ami.tf
+++ DataSources-Exercises/ami.tf
@@ -7,6 +7,36 @@ data "aws_ami" "retrieved_ami" {
 data "aws_caller_identity" "admin" {}
 data "aws_region" "region" {}
 
+// Manejando recursos mediante tags, no incluidos en el proyecto
+// Se creo una VPC manualmente desde la página de AWS que tiene el tag especificado aquí.
+data "aws_vpc" "console_managed" {
+  tags = {
+    Env = "Prod"
+  }  
+}
+
+data "aws_iam_policy_document" "get" {
+  statement {
+    sid = "PublicRead"
+    principals {
+      type = "*"
+      identifiers = ["*"]
+    }
+    actions = ["s3:GetObject"]
+
+    resources = [data.aws_vpc.console_managed.arn]
+  }  
+}
+
+output "policy" {
+  value = data.aws_iam_policy_document.get.json
+}
+
+output "console_managed" {
+  value = data.aws_vpc.console_managed.arn
+}
+
+
 output "ubuntu_ami_data" {
   value = data.aws_ami.retrieved_ami.id
 }
