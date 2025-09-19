b25e996746395795be66e64fcad04ea1b9289156
CarlosDRomero <cdavid.romeror@gmail.com>
2025-09-19 09:48:13 -0500

    Moviendo tags comunes a la configuración del provider

--- Variables2-Exercises/provider.tf
+++ Variables2-Exercises/provider.tf
@@ -10,4 +10,14 @@ terraform {
 
 provider "aws" {
   region = "us-east-2"
+  default_tags {
+    tags = local.common_tags
+  }
+}
+
+locals {
+  common_tags = {
+    Environment = "Dev"
+    Owner       = "CDROM"
+  }
 }
--- Variables2-Exercises/vpc.tf
+++ Variables2-Exercises/vpc.tf
@@ -1,32 +1,25 @@
-locals {
-  common_tags = {
-    Environment = "Dev"
-    Owner       = "CDROM"
-  }
-}
-
 resource "aws_vpc" "main" {
   cidr_block = var.cidr
 
-  tags = merge(local.common_tags, {
+  tags = {
     Name = "MAIN_VPC"
-  })
+  }
 }
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet
   map_public_ip_on_launch = true
 
-  tags = merge(local.common_tags, {
+  tags = {
     Name = "Public Subnet"
-  })
+  }
 }
 
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnet
 
-  tags = merge(local.common_tags, {
+  tags = {
     Name = "Private Subnet"
-  })
+  }
 }
